154bbc764bf2b05d99e761ef6595ae9cfed370a1
Matthew Christensen <matt.christensen@byu.edu>
2018-04-18 23:46:09 -0600

    removed cors

--- DinnerAndAMovie/settings.py
+++ DinnerAndAMovie/settings.py
@@ -52,13 +52,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'homepage',
 ]
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,8 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True 
 
 ROOT_URLCONF = 'DinnerAndAMovie.urls'
 
